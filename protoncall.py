6f1d509532fdf7f53816ca72d198645f9cdc01c3
caverym <caverym@outlook.com>
2020-09-22 22:04:38 -0400

    adding a class for easier variable management

--- protoncall.py
+++ protoncall.py
@@ -2,108 +2,114 @@
 import os
 import sys
 
+n = 'null'
 scdp = 'STEAM_COMPAT_DATA_PATH'
-custom = 'unknown'
-version = '0.4'
+custom = n
+version = '0.5a'
+_argv1 = sys.argv[1]
 
+# Set defaults
+err_val = 255
+proton = 5.0
+program = n
+proton_path = "~/.steam/steam/steamapps/common/Proton\\ 5.0/"
+_help = True
 
-# setup mode
 
 def help_message():
 	print(f"Proton Caller by Avery Murray, version: {version}.\n"
-		f"Use this to easily run any Windows executable using Steam's Proton\n\n"
-		f"Usage:\n [-c, -h][5, 5.0, 4.11, 4.3, 3.16, 3.7][./*.exe]\n"
-		f"'proton-call 5 ./foo.exe'\n'proton-call -c '/Proton\ 5.0/' ./foo.exe'\n\n"
-		f"Working directory must be the same as the Windows executable. Proton Caller will fail without arguments.")
-	sys.exit(a)
+	f"Use this to easily run any Windows executable using Steam's Proton\n\n"
+	f"Usage:\n [-c, -h][5, 5.0, 4.11, 4.3, 3.16, 3.7][./*.exe]\n"
+	f"'proton-call 5 ./foo.exe'\n'proton-call -c '/Proton\\ 5.0/' ./foo.exe'\n\n"
+	f"Working directory must be the same as the Windows executable. Proton Caller will fail without arguments.")
+	sys.exit(_vars.err_val)
 
 
 def proton_addition():
-	os.system('mkdir ~/proton')
-	os.system("echo 'export STEAM_COMPAT_DATA_PATH=$HOME/proton' >> ~/.profile")
-	print("If this did not work, manually add it to your shell profile.")
-	return
+	add_dir = input('Directory you will like {scdp} added.'
+					'this will add a directory.\nadd_dir: ')
+	os.system(f'mkdir {add_dir}/proton')
+	_profile = input('Shell profile file: ')
+	os.system(f"echo 'export STEAM_COMPAT_DATA_PATH={add_dir}/proton' > {_profile}")
+	_vars.scdp = f'{add_dir}/proton'
+	return _vars.scdp
 
-
-def proton_select():
-	prtn = sys.argv[1]
-	if prtn == '5':
-		prtn = '5.0'
-		return prtn
-	elif prtn > '3':
-		return prtn
-	else:
-		print('\n Select proton version.')
-		sys.exit(1)
-
-
-#  launcher mode
 
 def setup():
 	print(f'Custom mode: {custom}')
 	if scdp in os.environ:
-		print(f'{scdp} exists at: {os.environ[scdp]} \n')
+		print(f'{scdp} exists at {os.environ[scdp]}\b')
+		return
 	else:
 		print(f'{scdp} was not found')
-		add_var = input('Would you like this added? [y/n]: ')
+		add_var = input(f'Would you like to add {scdp}? [y/n]: ')
 		if add_var == 'y':
-			proton_addition()
-		elif add_var == 'yes':
-			proton_addition()
+			_vars.scdp = proton_addition()
+			return _vars.scdp
+		elif add_var == 'n':
+			sys.exit(f"Won't add {scdp}. Add it to your environment")
 		else:
-			print(f'Please add {scdp} to your environment variables')
-			sys.exit(1)
-
-
-def program_select():
-	if custom == 'True':
-		program = sys.argv[3]
-	else:
-		program = sys.argv[2]
-	print(program)
-	return program
-
-
-def proton_list():
-	print('Finding Proton installs…')
-	os.system('ls ~/.steam/steam/steamapps/common/ | grep Pr')
-	return
+			sys.exit(_vars.err_val)
 
 
 def proton_call():
+	print(f'\n {_vars}\n')
+	if _help:
+		help_message()
 	setup()
-	proton_list()
-	prtn = proton_select()
-	print(f'Using Proton {prtn}')
-	program = program_select()
-	os.system(f'~/.steam/steam/steamapps/common/Proton\ {prtn}/proton run {program}')
+	if custom:
+		print(f'Using Proton {_vars.proton}')
+		print(f'Program: {_vars.program}')
+		os.system(f'{_vars.proton_path}/proton run {_vars.program}')
+		return
+	elif not custom:
+		os.system(f'~/.steam/steam/steamapps/common/Proton\\ {_vars.proton}/proton run {_vars.program}')
+	else:
+		print(f'How did we get here?')
+		sys.exit(_vars.err_val)
 
 
-# custom mode
+class ProtonCaller:
+	def __init__(self, _argv1, custom, err_val, version, scdp, proton, program, proton_path, _help):
+		self._argv1 = _argv1
+		self.custom = custom
+		self.err_val = err_val
+		self.version = version
+		self.scdp = scdp
+		self.proton = proton
+		self. program = program
+		self.proton_path = proton_path
+		self._help = _help
 
-def custom_call():
-	setup()
-	proton_path = sys.argv[2]
-	program = program_select()
-	os.system(f'{proton_path}/proton run {program}')
-	sys.exit(0)
 
-
-# check mode
-
+pc = ProtonCaller
 if len(sys.argv[1]) > 4:
-	a = 1
-	help_message()
-elif sys.argv[1] == 'help':
-	a = 0
-	help_message()
-elif sys.argv[1] == '-h':
-	a = 0
-	help_message()
-elif sys.argv[1] == '-c':
-	custom = 'True'
-	custom_call()
-else:
-	custom = 'False'
+	_vars = pc(sys.argv[1], custom, err_val, version, scdp, proton, program, proton_path, _help)
+	print(_vars)
 	proton_call()
-
+elif sys.argv[1] == 'help':
+	err_val = 0
+	_vars = pc(sys.argv[1], custom, err_val, version, scdp, proton, program, proton_path, _help)
+	proton_call()
+elif sys.argv[1] == '-h':
+	err_val = 0
+	_vars = pc(sys.argv[1], custom, err_val, version, scdp, proton, program, proton_path, _help)
+	proton_call()
+elif sys.argv[1] == '-c':
+	_help = False
+	custom = True
+	proton_path = sys.argv[2]
+	program = sys.argv[3]
+	_vars = pc(sys.argv[1], custom, err_val, version, scdp, proton, program, proton_path, _help)
+	proton_call()
+else:
+	_help = False
+	if sys.argv[1] == '5':
+		program = sys.argv[2]
+		_vars = pc(sys.argv[1], custom, err_val, version, scdp, '5.0', program, proton_path, _help)
+		proton_call()
+	else:
+		proton = sys.argv[1]
+		program = sys.argv[2]
+		_vars = pc(sys.argv[1], custom, err_val, version, scdp, proton, program, proton_path, _help)
+		proton_call()
