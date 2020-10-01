3da32d502768a510777a4eb5ed0095bed13b1cb6
Avery Murray <avery@localhost.localdomain>
2020-10-01 17:04:09 -0400

    hm

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-mkdir /usr/share/licenses/proton-call
-cp LICENSE /usr/share/licenses/proton-call/
-pyinstaller --onefile protoncall.py
-cp dist/protoncall /usr/bin/proton-call
+mkdir -p /usr/share/licenses/proton-call
+cp -f LICENSE /usr/share/licenses/proton-call/
+cp -f protoncall.py /usr/bin/proton-call
 chmod +x /usr/bin/proton-call
 install -g 0 -o 0 -m 0644 proton-call.6 /usr/share/man/man6/
 gzip -f /usr/share/man/man6/proton-call.6
