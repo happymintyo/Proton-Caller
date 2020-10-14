f96cef4e3372a6d54443041e8b81ff633773819f
Avery Murray <caverym@outlook.com>
2020-10-14 17:17:37 -0400

    code cleanup

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
-/*
-DO NOT USE THIS, IT IS NOT FINISHED
-
-*/
-
 #include <iostream>
 #include <string>
 #include <cstring>
-
 using namespace std;
 
 
@@ -20,21 +14,21 @@ class ProtonClass {
         string program;
         string proton_path;
         bool custom{};
+        
 
+        void proton_call() {
+            string exec_ = proton_path + proton + "/proton run " + program;
+            char cmd[200];
+            strcpy(cmd, exec_.c_str());
+            system(cmd);
+        }
 };
-
-void proton_call(const ProtonClass& pc) {
-    string exec_ = pc.proton_path + pc.proton + "/proton run " + pc.program;
-    char cmd[200];
-    strcpy(cmd, exec_.c_str());
-    cout << endl << cmd << endl;
-    system(cmd);
-}
 
 
 void help() {
     cout << "Usage: [-c, -h][5, 5.0, 4.11, 4.3, 3.16, 3.7][./*.exe]\n 'proton-call 5 ./foo.exe'\n 'proton-call -c '/Proton\\ 5.0/' ./foo.exe'\n";
 }
+
 
 void checker(ProtonClass &pc) {
     string _home = getenv("HOME");
@@ -52,57 +46,29 @@ void checker(ProtonClass &pc) {
         pc.proton_path = _home + "/.steam/steam/steamapps/common/Proton\\ ";
         return;
     }
-
 }
 
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     ProtonClass pc;
     pc.steam = "STEAM_COMPAT_DATA_PATH";
-    string version = "1.0.1";
-
+    string version = "1.1.0";
     cout << "Proton Caller by Avery Murray version: " << version << "\n\n";
 
+    // check for compat data path
     if (getenv(pc.steam) == nullptr) {
-        cout << "Please add " << pc.steam << " to your environment:\n\nexport " << pc.steam << "=$HOME/proton/\n";
-        help();
-        exit (EXIT_FAILURE);
+        cout << "Please add " << pc.steam << " to your environment:\n\n export" << pc.steam << "=$HOME/proton/\n";
+        help();exit(EXIT_FAILURE);
     }
 
-    cout << pc.steam << " located at: " << getenv(pc.steam) << endl;
+    cout << pc.steam << " located at: " << getenv(pc.steam) << "\n\n";
+    if (argc == 1){help();exit(EXIT_FAILURE);}
+    if (argv[1] != nullptr){pc._argv1 = argv[1];}
+    if (argv[2] != nullptr){pc._argv2 = argv[2];}
+    if (argv[3] != nullptr){pc._argv3 = argv[3];}
 
-    if (argc == 1) {
-        cout << "Missing arguments\n";
-    }
-    if (argv[1] != nullptr) {
-        pc._argv1 = argv[1];
-        if (pc._argv1 == "-h") {
-            help();
-            exit (EXIT_SUCCESS);
-        }
-    } else {
-        cout << "Missing arguments.\n";
-        return 255;
-    }
-    if (argv[2] != nullptr) {
-        pc._argv2 = argv[2];
-    } else {
-        cout << "missing arguments\n";
-        return 255;
-    }
-    if (argv[3] != nullptr) {
-        pc._argv3 = argv[3];
-    }
-
-    // check for arguments
     checker(pc);
-
-    if (pc.custom) {
-        pc.program = argv[3];
-    }
-
-    proton_call(pc);
+    pc.proton_call();
 
     return 0;
-
 }
