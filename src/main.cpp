4b7cccae5639a2077bd6eea6f59cf3fc00c0964a
caverym <caverym@outlook.com>
2020-10-27 15:49:13 -0400

    mm

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,14 @@ int main(int argc, char *argv[]) {
     ProtonObject.steam = "STEAM_COMPAT_DATA_PATH";
     ProtonObject.common = "PC_COMMON";
     std::string version = "1.2.0";
-    std::cout << "Proton Caller by Avery Murray version: " << version << "\n";
+    std::cout << "Proton Caller by Avery Murray version: " << version << ".\n";
 
     // check for compat data path
     if (getenv(ProtonObject.steam) != nullptr) {
         std::cout<< ProtonObject.steam << " located at: " << getenv(ProtonObject.steam) << "\n";
     } else {
-        std::cout << "Please add " << ProtonObject.steam << " to your environment:\n export" << ProtonObject.steam << "=$HOME/proton/\n";
+        std::cout << "Please add " << ProtonObject.steam << " to your environment:\n "
+                     "export" << ProtonObject.steam << "=$HOME/proton/\n";
         exit(EXIT_FAILURE);
     }
 
